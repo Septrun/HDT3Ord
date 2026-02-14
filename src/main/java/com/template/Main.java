7720bc727b637fc371e4f0358f6f2b81445a10f3
Septrun <diegopr8917@gmail.com>
2026-02-13 20:30:21 -0600

    Se completó el Main con el menú principal y la lógica para seleccionar los algoritmos Sort.

--- src/main/java/com/template/Main.java
+++ src/main/java/com/template/Main.java
@@ -1,46 +1,90 @@
 package com.template;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * El Main es la clase principal que gestiona la interfaz del usuario y la ejecución de los algoritmos tipo sort.
+ * Esto permite al usuario seleccionar entre cinco algoritmos diferentes para ordenar datos leídos desde un archivo.
+ * @author Diego Ayala, David Berganza
+ * @version 1.0
+ */
 
 public class Main {
-    private static final int ARRAY_SIZE = 100000;
-    private static final int ITERATIONS = 1000;
+
+    /** 
+    * El archivo numeros.txt contiene los números que se generarán al azar.
+    */
+    private static final String FILENAME = "numeros.txt";
     
+    /**
+    * Se van a generar y a ordenar 3000 números.
+    */
+    private static final int DATA_SIZE = 3000;
+
+    /**
+     * Se genera el archivo de datos si el usuario no elegió uno previamente, solicita la elección del usuario y ejecuta el algoritmo elegido.
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
-        System.out.println("Starting profiling demo application...");
-        System.out.println("JDK Version: " + System.getProperty("java.version"));
-        
-        performBubbleSort();
-        
-        System.out.println("Application completed successfully.");
-    }
-    
-    private static void performBubbleSort() {
-        System.out.println("Performing bubble sort...");
-        Random random = new Random();
-        int[] array = new int[ARRAY_SIZE / 10];
-        
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(1000);
+
+        System.out.println("Iniciando programa...");
+
+        File file = new File(FILENAME);
+        if (!file.exists()) {
+            FileHandler.generateNumbersToFile(FILENAME, DATA_SIZE);
         }
-        
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    swap(array, j, j+1);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nBienvenido al programa Sort! Por favor, seleccione algoritmo:");
+        System.out.println("1. Gnome Sort");
+        System.out.println("2. Merge Sort");
+        System.out.println("3. Quick Sort");
+        System.out.println("4. Radix Sort");
+        System.out.println("5. Shell Sort");
+        System.out.print("Opción: ");
+
+        int choice = scanner.nextInt();
+
+        Integer[] data = FileHandler.readNumbers(FILENAME);
+
+        System.out.println("Ordenando " + data.length + " elementos...");
+
+        switch (choice) {
+            case 1:
+                SortingAlgorithms.gnomeSort(data);
+                break;
+
+            case 2:
+                SortingAlgorithms.mergeSort(data);
+                break;
+
+            case 3:
+                SortingAlgorithms.quickSort(data, 0, data.length - 1);
+                break;
+
+            case 4:
+                int[] primitive = FileHandler.toPrimitive(data);
+                SortingAlgorithms.radixSort(primitive);
+                for (int i = 0; i < primitive.length; i++) {
+                    data[i] = primitive[i];
                 }
-            }
+                break;
+
+            case 5:
+                SortingAlgorithms.shellSort(data);
+                break;
+
+            default:
+                System.out.println("Hubo un error. La opción es inválida");
+                scanner.close();
+                return;
         }
-        
-        System.out.println("Bubble sort completed.");
-    }
 
-    private static void swap(int[] array, int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp; 
-    }
+        FileHandler.saveSortedArray("ordenados.txt", data);
 
+        System.out.println("El programa Sort ha finalizado. Muchas gracias por usarlo!");
+        scanner.close();
+    }
 }
